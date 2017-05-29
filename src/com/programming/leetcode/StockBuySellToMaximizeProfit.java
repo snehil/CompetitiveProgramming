44553f424eebb9a9a36fd16a3e11e67984dd35f7
Wakchaure,Snehil <Snehil.Wakchaure1@Cerner.com>
2017-05-29 10:42:51 -0500

    Removed redundant code

--- src/com/programming/leetcode/StockBuySellToMaximizeProfit.java
+++ src/com/programming/leetcode/StockBuySellToMaximizeProfit.java
@@ -2,10 +2,9 @@ package com.programming.leetcode;
 
 import java.util.Arrays;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 /**
  *  Question:
+ * 		http://www.programcreek.com/2014/02/leetcode-best-time-to-buy-and-sell-stock-java/
  * 
  * 	Say you have an array for which the ith element is the price of a given stock 
  *  on day i. If you were only permitted to complete at most one transaction 
